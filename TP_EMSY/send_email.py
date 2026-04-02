c550178a2b12a6d84cee578c536547c4837593ec
Luc Derré <luc.derre@eduvaud.ch>
2026-04-02 16:20:58 +0200

    Update send_email.py

--- TP_EMSY/send_email.py
+++ TP_EMSY/send_email.py
@@ -2,57 +2,77 @@ __author__ = "Pouly Steeve"
 __version__ = "0.1"
 __maintainer__ = "Pouly Steeve"
 __email__ = "steve.pouly@eduvaud.ch"
-__status__ = "Production"
-__date__ = "avril 2026"
+__status__ = "Prototype"
+__date__ = "Avril 2026"
 
 #-----------------------------------------------------
 # Importing libraries and modules
 #-----------------------------------------------------
 import datetime                                                             # Library for date and time related stuff
-import smtplib                                                              # Library for email related stuff
+import math                                                                 # Library for math stuff
+import csv                                                                  # Library for csv handling stuff
+
+from sensirion_i2c_driver import I2cConnection                              # Sensor driver
+from sensirion_i2c_sht.sht4x import Sht4xI2cDevice                          # Sensor driver
+from sensirion_i2c_driver.linux_i2c_transceiver import LinuxI2cTransceiver  # Sensor driver
+
+
+#-----------------------------------------------------
+# Declaring the sensor object
+#-----------------------------------------------------
+sht40 = Sht4xI2cDevice(I2cConnection(LinuxI2cTransceiver('/dev/i2c-2')))
 
 #-----------------------------------------------------
 # Declaring functions
 #-----------------------------------------------------
-def send_email(receiver, subject, message):
-    server = smtplib.SMTP("mail.infomaniak.com:587")
-    server.login("steve.pouly@etml-es.ch","y8wfWCzJsV2OGPvV")
-    sender = "steve.pouly@etml-es.ch"
 
-    headers = {
-        'Content-Type': 'text/html; charset=utf-8',
-        'Content-Disposition': 'inline',
-        'Content-Transfer-Encoding': '8bit',
-        'From': sender,
-        'To':receiver,
-        'Date': datetime.datetime.now().strftime('%a, %d %b %Y  %H:%M:%S %Z'),
-        'X-Mailer': 'python',
-        'Subject': subject
-    }
-    # create the message
-    msg = ''
-    for key, value in headers.items():
-        msg += "%s: %s\n" % (key, value)
-
-    # add contents
-    msg += "\n%s\n" % (message)
-
+def read_sensor():     #fonction d'utilisation des capteur
     try:
-        server.sendmail(headers['From'], headers['To'], msg.encode("utf8"))
-        server.quit()
-        print("Email sent successfully!")
+        t, rh = sht40.single_shot_measurement()
+        # Watch out! t and rh are variable that contain not only the values but also the units.
+        # You can print the values with the units (print(t)) or you can also recover only the value
+        # by specifying which one: t.degrees_celsius or rh.percent_rh
     except Exception as ex:
-        print("Something went wrong….", ex)
+        print("Error while recovering sensor values:", ex)
+    else:
+        return t, rh
 
+    return 0 # In case something went wrong
 
+def calculate_dew_point(temp, humidity):
+     # Calculate dew point here
+    dp      : float = 0      #Point rosé
+    Beta    : float = 17.62  #Coefficient
+    Lamda   : float= 243.12  #Coefficient
+    cent    : float = 100
+
+    dp = (Lamda * (math.log(humidity/cent) + (Beta * temp / (Lamda + temp)))) / (Beta - (math.log(humidity/cent) + Beta * temp / (Lamda + temp)))   #Calcul du point de rosée
+
+    return dp   #retour du résultat
+
+def csv_write_row(file_path, data_row): #Fonction d'écriture dans le dossier .CSV
+     try:
+     # Write csv here
+        with open('TempLog.csv', 'a', newline='', encoding='utf-8') as f:    #Ouverture du dossier en mode écriture SANS effacer ce qui est déjà écrit en retour à la ligne
+            writer = csv.writer(f, delimiter=',')                            #écrit dans le dossier avec une "," comme séparation des infos
+            writer.writerow(data_row)                                        #écriture des informations données
+            return 1
+     except Exception as ex:
+         print("erreur", ex)
+         return 0
+ 
 #-----------------------------------------------------
 # Main script
 #-----------------------------------------------------
 if __name__ == "__main__":  # Runs only if called as a script but not if imported
-    print("Hello This script is a test and will try to send an email")
-
-    # Sending an email to test the function
-    subject = "Ceci est un email de test"
-    message = "Bonjour! Ceci est un email de test."
-
-    send_email(["steve.pouly@etml-es.ch"], subject, message)
+    print("Hello and welcome to EMSY")
+    temperature, humidity = read_sensor()                                                                        #Appel de la fonction de lecture de température et humidité
+    dew_point = calculate_dew_point(temperature.degrees_celsius, humidity.percent_rh)                            #Appel de la fonction de calcul en donnant les informations
+    print("Temperature :", temperature)                                                                          #Afficher la temperature
+    print("Humidite relative :", humidity)                                                                       #Afficher la valeur de l'humidite relative
+    print("Point de Rosee :", round(dew_point, 2), "°C")                                                         #Afficher le point de rosée
+    
+    date_heur = datetime.datetime.now()                                                                          #Lecture de la date
+    ligne = [date_heur.strftime("%d-%m-%Y"), date_heur.strftime("%H:%M"), temperature, humidity, dew_point]      #Met tous les inofrmation dans un tableau
+    csv_write_row('TempLog.csv', ligne)                                                                          #Appel de la fonction d'automatisation avec le nom du dossier et le tableau comme infos d'entrée
+    
